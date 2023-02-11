7f33ea9cecb042ea7d5ae62ee9c21b53480f96e7
Chris <chrisloog120@gmail.com>
2023-02-11 22:34:26 +0200

    add Kata solution

---
+++ java/Kata.java
@@ -0,0 +1,12 @@
+public class Kata{
+
+    public static void main(String[] args) {
+        System.out.println(binaryAddition(1, 1));
+        System.out.println(binaryAddition(5, 9));
+    }
+  
+    public static String binaryAddition(int a, int b){
+      System.out.println(String.format("(%s + %s = %s in decimal or %s in binary)", a, b, a + b, Integer.toBinaryString(a + b)));  
+      return Integer.toBinaryString(a + b);
+    }
+  }
