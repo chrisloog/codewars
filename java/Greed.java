856166d817278da32e5584855395091a69a977ea
Chris <chrisloog120@gmail.com>
2023-03-01 13:38:36 +0200

    Add Greed implementation

---
+++ java/Greed.java
@@ -0,0 +1,21 @@
+public class Greed {
+
+    public static int greedy(int[] dice) {
+        int[] counts = new int[7]; // initialize counts to all zeros
+        for (int value : dice) {
+            counts[value]++;
+        }
+        int score = 0;
+        // Check for triplets of 1-6 and add corresponding points to score
+        for (int i = 1; i <= 6; i++) {
+            if (counts[i] >= 3) {
+                score += i == 1 ? 1000 : i * 100;
+                counts[i] -= 3;
+            }
+        }
+        // Add points for individual 1's and 5's
+        score += counts[1] * 100 + counts[5] * 50;
+        return score;
+    }
+
+}
