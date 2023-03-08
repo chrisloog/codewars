69f0d65f61d7785399a4d108d913f93a2a5f21e3
Chris <chrisloog120@gmail.com>
2023-03-08 19:04:25 +0200

    Refactor FindOutlier solution

--- java/FindOutlier.java
+++ java/FindOutlier.java
@@ -1,21 +1,12 @@
+import static java.util.Arrays.stream;
+
 public class FindOutlier{
 
     // https://www.codewars.com/kata/5526fc09a1bbd946250002dc
 
     public static int find(int[] integers) {
-        int evenCount = 0, oddCount = 0, even = 0, odd = 0;
-        for (int num : integers) {
-            if (num % 2 == 0) {
-                evenCount++;
-                even = num;
-            } else {
-                oddCount++;
-                odd = num;
-            }
-            if (evenCount > 1 && oddCount == 1) return odd;
-            if (oddCount > 1 && evenCount == 1) return even;
-        }
-        return evenCount == 1 ? even : odd;
+        final int p = stream(integers).limit(3).map(Math::abs).map(i -> i % 2).sum() / 2;
+		return stream(integers).filter(i -> Math.abs(i) % 2 != p).findFirst().getAsInt();
     }
-        
+
 }
