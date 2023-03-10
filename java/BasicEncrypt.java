d2173e08794a16d617e56fb82c33b745d88431b7
Chris <chrisloog120@gmail.com>
2023-03-10 16:25:15 +0200

    Refactor BasicEncrypt solution

--- java/BasicEncrypt.java
+++ java/BasicEncrypt.java
@@ -3,20 +3,11 @@ public class BasicEncrypt {
     // https://www.codewars.com/kata/5862fb364f7ab46270000078
 
     public static String encrypt(String text, int rule) {
-        StringBuilder encryptedTextBuilder = new StringBuilder();
-        for (int i = 0; i < text.length(); i++) {
-            int asciiValue = getWrappedAsciiValue(text.charAt(i) + rule);
-            encryptedTextBuilder.append((char)asciiValue);
-        }
-        return encryptedTextBuilder.toString();
-    }
-    
-    private static int getWrappedAsciiValue(int asciiValue) {
-        int wrappedAsciiValue = asciiValue % 256;
-        if (wrappedAsciiValue < 0) {
-            wrappedAsciiValue += 256;
-        }
-        return wrappedAsciiValue;
-    }
+        StringBuilder encrypted = new StringBuilder();
+		for(Character a: text.toCharArray()){
+			encrypted.append(Character.toChars((a + rule) % 256)) ;
+		}
+	    return encrypted.toString(); 
+    }  
      
 }
